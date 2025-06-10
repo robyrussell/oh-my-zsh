8c2ab723074c724281146c1e4381c754f24a5a95
robyrussell <meadowzach6@gmail.com>
2025-06-10 12:05:52 -0400

    Update example.zsh

--- custom/example.zsh
+++ custom/example.zsh
@@ -1,10 +1,10 @@
 # Put files in this folder to add your own custom functionality.
-# See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization
+# For example: add shortcuts to your local projects. See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization
 #
 # Files in the custom/ directory will be:
 # - loaded automatically by the init script, in alphabetical order
 # - loaded last, after all built-ins in the lib/ directory, to override them
-# - ignored by git by default
+# - now loaded by git commit
 #
 # Example: add custom/shortcuts.zsh for shortcuts to your local projects
 #
